3b4691a0ea60924873e965d53eb6b3f7c2d010f0
srmarcballestero <marconiballestero@gmail.com>
2020-09-27 12:27:07 +0200

    Misc

--- Source/PosicioBatec.py
+++ Source/PosicioBatec.py
@@ -26,35 +26,41 @@ nom_directori = "/home/marc/OneDrive/Documents/Universitat/Física/S4 - Mecànic
 
 noms_simulacions = Path(nom_directori + "/Data/").glob("*Sim*")
 
-for nom_simulacio in noms_simulacions:
+nom_figura = Path(nom_directori+"Amplituds/")
+try:
+    nom_figura.mkdir(parents=True, exist_ok=False)
+except FileExistsError:
+    pass
+
+t_vs_gamma = np.empty((10, 2))
+
+for iter, nom_simulacio in enumerate(noms_simulacions):
     """
     Variables caracterísitques del sistema i generació de l'objecte
     """
     nom_simulacio = str(basename(nom_simulacio)).replace(".csv", "")
     nom_simulacio = nom_simulacio.replace("_Sim", "")
-    nom_data = nom_directori + "Data/" + nom_simulacio + "_Sim.csv"
+    nom_amplituds = nom_directori + "Amplituds/" + nom_simulacio + "_Amp.csv"
     nom_metadata = nom_directori + "Metadata/" + nom_simulacio + "_Sim.dat"
 
     parametres_sist = sim.llegeixMetadata(nom_metadata)
 
     sist = sim.Sistema(**parametres_sist)
 
-    t = sim.Data(nom_data).temps
-    pos = sim.Data(nom_data).posicions
+    data = np.genfromtxt(nom_amplituds, delimiter=",")
+
+    t = data[:, 0]
+    amps = data[:, 1]
 
     """
-    Detecció i càlcul de l'amplitud dels màxims
+    Detecció del primer mínim en les amplituds
     """
-    j = 0
-    maxims = np.zeros(int(sist.num_osc))
-    t_max = np.zeros(int(sist.num_osc))
-
-    for i in range(np.size(maxims)):
-        while t[j] < (i+1)*sist.T0 and j < np.size(t)-1:
-            if np.fabs(pos[j, 1]) > maxims[i]:
-                maxims[i] = np.fabs(pos[j, 1])
-                t_max[i] = t[j]
-            j += 1
+    for i in range(1, len(t)-1):
+        if amps[i] - amps[i-1] < 0 and amps[i+1] - amps[i] > 0:
+            t_batec = t[i]
+            break
+        else:
+            pass
 
     """
     Representació gràfica de les dades
@@ -63,24 +69,18 @@ for nom_simulacio in noms_simulacions:
                       + cycler(linestyle=['-', '--', ':', '-.', '--']))
     plt.rc('axes', prop_cycle=default_cycler)
 
-    plt.xlabel("t/T0 (-)", fontsize=18)
-    plt.ylabel("x/A (-)", fontsize=18)
+    plt.xlabel("$\gamma$ ($\mathrm{kg}/\mathrm{s} \mathrm{m}^2}$)", fontsize=18)
+    plt.ylabel("$t/T_0$ (-)", fontsize=18)
     # plt.legend(loc="upper right")
 
-    plt.plot(np.array(range(1, int(sist.num_osc)+1)), maxims/np.max(sist.A), '.', color='blue')
+    t_vs_gamma[iter] = np.array([sist.gamma, t_batec/sist.T0])
 
-    nom_figura = Path(nom_directori+"Amplituds/")
-    try:
-        nom_figura.mkdir(parents=True, exist_ok=False)
-    except FileExistsError:
-        pass
+    plt.plot(sist.gamma, t_batec/sist.T0, '.', color='blue')
+    print("Gamma = %.2e" % (sist.gamma))
 
-    print("Desant figura %s" % (nom_simulacio+"_Amp.png"))
 
-    with open(str(nom_figura)+"/"+nom_simulacio+"_Amp.csv", "w") as fitxer:
-        for i in range(len(maxims)):
-            fitxer.write("%e,%e\n" % (t_max[i], maxims[i]))
-
-    plt.savefig(str(nom_figura)+"/"+nom_simulacio+"_Amp.png")
-    # plt.show()
-    plt.clf()
+plt.savefig(str(nom_figura)+"/"+nom_simulacio+"_GammaVsBatec.png")
+print(t_vs_gamma[:, 0])
+print(t_vs_gamma[:, 1])
+# plt.show()
+plt.clf()
